900872e640048dddbe41d28394c130371887f8ed
Lodder <zaloddjo@ZAMPC812.local>
2020-05-12 09:51:21 +0200

    Simplify + tweaks

---
+++ provider/local/create.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+set -euo pipefail # Stop running the script on first error...
+# -----------------------------------------------------------------------------
+# Install docker for desktop:
+#  - https://www.docker.com/products/docker-desktop
+# -----------------------------------------------------------------------------
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/../../config/local.env
+
+# Initialise tooling and local kubernetes cluster
+#  -> https://www.techrepublic.com/article/how-to-add-kubernetes-support-to-docker-desktop/
+
+
+# In local mode, we will not create any image repositories
+# -> No image registry needed for local.
+
+
+# Make sure we are configured to point to the correct cluster
+kubectl config use-context docker-desktop
+
+
+# Docker for desktop is a single node cluster. 
+# -> Cannot scale to more than one...
+
+
+kubectl get nodes
---
+++ provider/local/delete.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+set -euo pipefail # Stop running the script on first error...
+# -----------------------------------------------------------------------------
+# Install docker for desktop:
+#  - https://www.docker.com/products/docker-desktop
+# -----------------------------------------------------------------------------
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source $DIR/../../config/local.env
+
+
+# No cluster and/or image repository to delete
+echo "Noting to delete"
