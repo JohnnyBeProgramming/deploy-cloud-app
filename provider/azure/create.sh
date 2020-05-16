1a14738c1e658630bfcf6eb151b7c9d316b14e83
Lodder <zaloddjo@ZAMPC812.local>
2020-05-16 14:38:43 +0200

    Tweaks for azure provider

--- provider/azure/create.sh
+++ provider/azure/create.sh
@@ -6,11 +6,14 @@ set -euo pipefail # Stop running the script on first error...
 #  - http://azure.microsoft.com/
 # -----------------------------------------------------------------------------
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $DIR/../../config/azure/cloud.env
+CFG="$( cd $DIR/../../config/ && pwd )/azure"
+source $CFG/cloud.env
 
+# Define new deployment settings
+echo "" > $CFG/deploy.ini
 
 # Login to Azure account
-az --version
+# az --version
 az login
 
 # Install required providers from microsoft
@@ -19,43 +22,58 @@ az provider register --namespace Microsoft.Compute
 az provider register --namespace Microsoft.OperationsManagement
 
 # List and select an account
-az account list | jq -r ".[] | .name"
-az account set --subscription "Free Trial"
+# az account list | jq -r ".[] | .name"
+az account set --subscription "${CLUSTER_SUBSCRIPTION}"
 
 # List and select a default region
-az account list-locations | jq -r ".[] | .name"
+# az account list-locations | jq -r ".[] | .name"
 
 # Create our resource group where we will deploy our app
-az group create --name ${CLUSTER_TARGET} --location ${CLUSTER_REGION}
+az group create --name ${CLUSTER_TARGET} --location ${CLUSTER_REGION} \
+| jq .properties.provisioningState
+
+# Provision a disk for our cluster
+az disk create \
+--resource-group ${CLUSTER_TARGET} \
+--name "demo-volume" \
+--sku "Standard_LRS" \
+--os-type "Linux" \
+--size-gb 10 \
+| jq .provisioningState
 
 # Create an Azure ACR registry to store docker containers
 az acr create \
-    --location ${CLUSTER_REGION} \
-    --resource-group ${CLUSTER_TARGET} \
-    --name ${CLUSTER_TARGET} \
-    --sku "Basic" \
-  | jq .provisioningState
+--location ${CLUSTER_REGION} \
+--resource-group ${CLUSTER_TARGET} \
+--name ${CLUSTER_TARGET} \
+--sku "Basic" \
+| jq .provisioningState
 
 # Login to ACR registry
 az acr login --name ${CLUSTER_TARGET}
 
 # Create a new AKS Cluster (single node)
 az aks create \
-    --name ${CLUSTER_TARGET} \
-    --resource-group ${CLUSTER_TARGET} \
-    --node-vm-size="Standard_A4m_v2" \
-    --node-count 1 \
- | jq .provisioningState
+--name ${CLUSTER_TARGET} \
+--resource-group ${CLUSTER_TARGET} \
+--node-vm-size="Standard_A4m_v2" \
+--node-count 1 \
+| jq .provisioningState
 
-# Assign a role to allow image pulls from our new cluster
-az role assignment create \
-    --assignee $(az aks show --query servicePrincipalProfile.clientId -n ${CLUSTER_TARGET} -g ${CLUSTER_TARGET} -o tsv) \
-    --scope $(az acr show --query id -n ${CLUSTER_TARGET} -g ${CLUSTER_TARGET} -o tsv) \
-    --role "acrpull" \
- | jq .scope
+# Assign a role to allow image pulls from ACR to our new cluster
+assignee=$(az aks show --query servicePrincipalProfile.clientId -n ${CLUSTER_TARGET} -g ${CLUSTER_TARGET} -o tsv)
+scope=$(az acr show --query id -n ${CLUSTER_TARGET} -g ${CLUSTER_TARGET} -o tsv)
+az role assignment create --assignee $assignee --scope $scope --role "acrpull" | jq .scope
+
+# Assign a role for the cluster to access the storage resource
+assignee=$(az aks show --query servicePrincipalProfile.clientId -n ${CLUSTER_TARGET} -g ${CLUSTER_TARGET} -o tsv)
+scope=$(az disk show --resource-group ${CLUSTER_TARGET} --name "demo-volume" --query id -o tsv)
+az role assignment create --assignee $assignee --scope $scope --role Contributor | jq .scope
+echo "storage.volume.azureDisk.diskURI=$scope" >> $CFG/deploy.ini
+
 
 # Enable monitoring on the cluster
-az aks enable-addons -a monitoring --name ${CLUSTER_TARGET} --resource-group ${CLUSTER_TARGET} | jq .fqdn || true
+# az aks enable-addons -a monitoring --name ${CLUSTER_TARGET} --resource-group ${CLUSTER_TARGET} | jq .fqdn || true
 
 # Configure local kubectl to connect to our AKS cluster
 az aks get-credentials --name ${CLUSTER_TARGET} --resource-group ${CLUSTER_TARGET}
--- provider/azure/destroy.sh
+++ provider/azure/destroy.sh
@@ -5,7 +5,7 @@ set -euo pipefail # Stop running the script on first error...
 # Register an Azure account for free:
 #  - http://azure.microsoft.com/
 # -----------------------------------------------------------------------------
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" 2>&1 > /dev/null && pwd )"
 source $DIR/../../config/azure/cloud.env
 
 
@@ -13,11 +13,40 @@ source $DIR/../../config/azure/cloud.env
 az --version
 az login
 
-# Delete the AKS Cluster 
-az aks delete -n ${CLUSTER_TARGET} -g ${CLUSTER_TARGET}
+
+# Delete the AKS Cluster
+if az aks show -n ${CLUSTER_TARGET} -g ${CLUSTER_TARGET} > /dev/null 2>&1
+then
+    az aks delete -n ${CLUSTER_TARGET} -g ${CLUSTER_TARGET}
+fi
+
 
 # Delete the ACR Image Repository
-az acr repository delete -n ${CLUSTER_TARGET} --repository ${ARTIFACT}
+if az acr repository show -n ${CLUSTER_TARGET} --repository ${ARTIFACT} > /dev/null 
+then
+    az acr repository delete -n ${CLUSTER_TARGET} --repository ${ARTIFACT}
+fi
+# az acr delete -n ${CLUSTER_TARGET} --resource-group ${CLUSTER_TARGET}
+
+
+# Delete the Azure Disk
+if az disk show --resource-group ${CLUSTER_TARGET} --name demo-volume > /dev/null 2>&1
+then
+    az disk delete \
+    --resource-group ${CLUSTER_TARGET} \
+    --name demo-volume
+fi
+
 
 # Delete the entire resource group
-az group delete -n ${CLUSTER_TARGET}
+if az group show -n ${CLUSTER_TARGET} > /dev/null 2>&1
+then
+    az group delete -n ${CLUSTER_TARGET}
+fi
+
+
+# Remove the deployment parameters
+if [ -f $DIR/../../config/azure/deploy.env ]
+then
+    rm -f $DIR/../../config/azure/deploy.env
+fi
